73776bbe7805c115f8e2c52368aa8b3a00c7c091
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-08-14 18:32:13 +0800

    fix: close db properly regardless of test result

--- tests/mocks.py
+++ tests/mocks.py
@@ -15,10 +15,12 @@ def bind_test_database(function: callable, *args, **kwargs) -> callable:
     """Decorator to open and close a test database connection"""
 
     def wrapper():
-        mock_db.bind(models)
-        mock_db.create_tables([*models, *relational_models])
-        function(*args, **kwargs)
-        mock_db.drop_tables([*models, *relational_models])
-        mock_db.close()
+        try:
+            mock_db.bind(models)
+            mock_db.create_tables([*models, *relational_models])
+            function(*args, **kwargs)
+        finally:
+            mock_db.drop_tables([*models, *relational_models])
+            mock_db.close()
 
     return wrapper

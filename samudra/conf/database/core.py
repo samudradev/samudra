740d38aaa99903521f3538e8594c3bf456ebb2c5
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-10-21 09:59:32 +0800

    ♻ REFACTOR: Extract create_sqlite method

--- samudra/conf/database/core.py
+++ samudra/conf/database/core.py
@@ -32,74 +32,21 @@ def get_database(db_name: str, engine: DatabaseEngine, **kwargs) -> pw.Database:
                 engine, "\n - ".join(DatabaseEngine.__members__.values())
             )
         )
-
-    # Environment variables
-    if engine is not DatabaseEngine.SQLite:
-        DATABASE_HOST = os.getenv("DATABASE_HOST")
-        DATABASE_PORT = int(os.getenv("DATABASE_PORT"))
-        DATABASE_OPTIONS = os.getenv("DATABASE_OPTIONS")
-        USERNAME = os.getenv("DATABASE_USERNAME")
-        PASSWORD = os.getenv("DATABASE_PASSWORD")
-        SSL_MODE = os.getenv("SSL_MODE")
-
     if engine == DatabaseEngine.SQLite:
-        sqlite_db_name: str = "samudra.db"
-        path: str = kwargs.pop("path")
-        # Defaults to make it async-compatible (according to FastAPI/Pydantic)
-        class PeeweeConnectionState(pw._ConnectionState):
-            def __init__(self, **kwargs):
-                super().__setattr__("_state", db_state)
-                super().__init__(**kwargs)
+        return create_sqlite(folder=db_name, path=kwargs.pop("path"), **kwargs)
 
-            def __setattr__(self, name, value):
-                self._state.get()[name] = value
+    DATABASE_HOST = os.getenv("DATABASE_HOST")
+    DATABASE_PORT = int(os.getenv("DATABASE_PORT"))
+    DATABASE_OPTIONS = os.getenv("DATABASE_OPTIONS")
+    USERNAME = os.getenv("DATABASE_USERNAME")
+    PASSWORD = os.getenv("DATABASE_PASSWORD")
+    SSL_MODE = os.getenv("SSL_MODE")
 
-            def __getattr__(self, name):
-                return self._state.get()[name]
-
-        # The DB connection object
-        # ? Perlu ke test?
-        # TODO Add Test
-        base_path: Path = Path(path, db_name)
-        full_path: Path = Path(base_path, sqlite_db_name)
-        try:
-            base_path.mkdir(parents=True)
-        except FileExistsError:
-            if full_path in [*base_path.iterdir()]:
-                raise FileExistsError(
-                    f"A samudra database already exists in {full_path.resolve()}"
-                )
-            elif [*base_path.iterdir()] is [None]:
-                print(
-                    f"Populating empty folder `{base_path.resolve()}` with {sqlite_db_name}"
-                )
-            else:
-                raise FileExistsError(
-                    f"The path `{base_path.resolve()}` is already occupied with something else. Consider using other folder."
-                )
-        # Set up readme
-        README = Path(base_path, "README.md")
-        README.touch()
-        with README.open(mode="w") as f:
-            f.writelines(
-                [
-                    f"# {db_name.title()}\n",
-                    "Created using [samudra](https://github.com/samudradev/samudra)",
-                ]
-            )
-        return_db = pw.SqliteDatabase(
-            full_path.resolve(),
-            check_same_thread=False,
-        )
-        return_db._state = PeeweeConnectionState()
-
-        logging.info(f"Connecting to {return_db.database}")
-
-    elif engine == DatabaseEngine.MySQL:
+    if engine == DatabaseEngine.MySQL:
         conn_str = f"mysql://{USERNAME}:{PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{db_name}?ssl-mode=REQUIRED"
         return_db = pw.MySQLDatabase(conn_str)
         logging.info(f"Connecting to {return_db.database} as {USERNAME}")
-    elif engine == DatabaseEngine.CockroachDB:
+    if engine == DatabaseEngine.CockroachDB:
         from playhouse.cockroachdb import CockroachDatabase
 
         conn_str = f"postgresql://{USERNAME}:{PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{db_name}?sslmode=verify-full&options={DATABASE_OPTIONS}"
@@ -107,4 +54,54 @@ def get_database(db_name: str, engine: DatabaseEngine, **kwargs) -> pw.Database:
         logging.info(f"Connecting to {return_db.database} as {USERNAME}")
     else:
         raise NotImplementedError("Invalid engine")
+    return return_db
+
+
+def create_sqlite(folder: str, path: str, db_file: str = "samudra.db"):
+    # Defaults to make it async-compatible (according to FastAPI/Pydantic)
+    class PeeweeConnectionState(pw._ConnectionState):
+        def __init__(self, **kwargs):
+            super().__setattr__("_state", db_state)
+            super().__init__(**kwargs)
+
+        def __setattr__(self, name, value):
+            self._state.get()[name] = value
+
+        def __getattr__(self, name):
+            return self._state.get()[name]
+
+    # The DB connection object
+    # ? Perlu ke test?
+    # TODO Add Test
+    base_path: Path = Path(path, folder)
+    full_path: Path = Path(base_path, db_file)
+    try:
+        base_path.mkdir(parents=True)
+    except FileExistsError:
+        if full_path in [*base_path.iterdir()]:
+            raise FileExistsError(
+                f"A samudra database already exists in {full_path.resolve()}"
+            )
+        elif [*base_path.iterdir()] is [None]:
+            print(f"Populating empty folder `{base_path.resolve()}` with {db_file}")
+        else:
+            raise FileExistsError(
+                f"The path `{base_path.resolve()}` is already occupied with something else. Consider using other folder."
+            )
+    # Set up readme
+    README = Path(base_path, "README.md")
+    README.touch()
+    with README.open(mode="w") as f:
+        f.writelines(
+            [
+                f"# {folder.title()}\n",
+                "Created using [samudra](https://github.com/samudradev/samudra)",
+            ]
+        )
+    return_db = pw.SqliteDatabase(
+        full_path.resolve(),
+        check_same_thread=False,
+    )
+    return_db._state = PeeweeConnectionState()
+    logging.info(f"Connecting to {return_db.database}")
     return return_db
